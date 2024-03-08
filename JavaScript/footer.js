5a70a991ae7308479502a8f079723a80638e07f0
João Pedro <107947773+joaopedromribeiro13@users.noreply.github.com>
2024-03-08 16:51:38 -0300

    Commit inicial

---
+++ JavaScript/footer.js
@@ -0,0 +1,9 @@
+function scrollDown(){
+    window.scroll({
+              top: document.body.scrollHeight,
+              behavior: 'smooth'
+          });
+  }
+
+
+// ESTA FUNÇÃO SERVE PARA ESCROLAR ATÉ O FINAL DA TELA
--- footer.js
+++
@@ -1,9 +0,0 @@
-function scrollDown(){
-    window.scroll({
-              top: document.body.scrollHeight,
-              behavior: 'smooth'
-          });
-  }
-
-
-// ESTA FUNÇÃO SERVE PARA ESCROLAR ATÉ O FINAL DA TELA
